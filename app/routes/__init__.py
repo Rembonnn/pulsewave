6dceb4f2a495ca8454ad9bd1be65ad8a332cee25
wirandra.rembon <alayarahman07@gmail.com>
2024-08-11 12:43:49 +0700

    feat(11/08/2024): authentication features using JWT and protect routes using middleware

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,14 @@
 from flask import Blueprint, jsonify
-from app.controllers import user_controller
+from app.controllers import user_controller, auth_controller
+from app.middleware.auth_middleware import token_required
 
 def init_routes(app):
-    app.add_url_rule('/api/users', 'create_user', user_controller.create_user, methods=['POST'])
-    app.add_url_rule('/api/users', 'get_users', user_controller.get_users, methods=['GET'])
-    app.add_url_rule('/api/users/<int:id>', 'get_user_by_id', user_controller.get_user_by_id, methods=['GET'])
-    app.add_url_rule('/api/users/<int:id>', 'update_user', user_controller.update_user, methods=['PUT'])
-    app.add_url_rule('/api/users/<int:id>', 'delete_user', user_controller.delete_user, methods=['DELETE'])
+    # Routes tanpa otentikasi JWT
+    app.add_url_rule('/api/login', 'login', auth_controller.login_user, methods=['POST'])
+    
+    # Routes yang dilindungi dengan otentikasi JWT
+    app.add_url_rule('/api/users', 'create_user', token_required(user_controller.create_user), methods=['POST'])
+    app.add_url_rule('/api/users', 'get_users', token_required(user_controller.get_users), methods=['GET'])
+    app.add_url_rule('/api/users/<int:id>', 'get_user_by_id', token_required(user_controller.get_user_by_id), methods=['GET'])
+    app.add_url_rule('/api/users/<int:id>', 'update_user', token_required(user_controller.update_user), methods=['PUT'])
+    app.add_url_rule('/api/users/<int:id>', 'delete_user', token_required(user_controller.delete_user), methods=['DELETE'])
